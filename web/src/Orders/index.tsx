760983835976a171564069730344220f1982481a
teteu <mat358591@gmail.com>
2021-01-09 17:34:26 -0300

    commit 09/01

--- web/src/Orders/index.tsx
+++ web/src/Orders/index.tsx
@@ -1,24 +1,79 @@
 import './styles.css'
+import { toast} from 'react-toastify'
 import StepsHeader from './StepsHeader'
 import ProductsList from './ProductsList'
 import { useEffect, useState } from 'react'
-import { Product } from './types'
-import { fetchProducts } from '../api'
+import { OrderLocationData, Product } from './types'
+import { fetchProducts, saveOrder } from '../api'
+import OrderLocation from './OrderLocation'
+import OrderSummary from './OrderSummary'
+import Footer from '../Footer'
+import { checkIsSelected } from './helpers'
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([])
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([])
+    const [orderLocation,setOrderLocation] = useState<OrderLocationData>()
+
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+        return sum + item.price
+    }, 0)
 
     useEffect(()=>{
         fetchProducts()
         .then(resp => setProducts(resp.data))
-        .catch(error => console.error(error))
+        .catch(() => {
+            toast.warning('Houve um erro ao buscar os produtos');
+        })
     },[])
 
+      
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts,product)
+        //verifica se ja está selecionado
+
+        if (isAlreadySelected) {
+            const selected = selectedProducts.filter(item => item.id !== product.id);
+            setSelectedProducts(selected);
+        } else {
+            setSelectedProducts(previous => [...previous, product]);
+        }
+    }
+  
+    const handleSubmit = () => {
+        const productsIds = selectedProducts.map(({ id }) => ({ id }));
+        const payload = {
+        ...orderLocation!,
+        products: productsIds
+        }
+    
+        saveOrder(payload).then((resp) => {
+            toast.error(`Pedido enviado com sucesso! N° ${resp.data.id}`);
+            setSelectedProducts([]);
+        })
+        .catch(() => {
+            toast.warning('Erro ao enviar pedido');
+        })
+    }
+
     return (
-        <div className="orders-container">
-            <StepsHeader/>
-            <ProductsList products={products}/>
-        </div>
+        <>
+            <div className="orders-container">
+                <StepsHeader/>
+                <ProductsList 
+                    products={products} 
+                    onSelectProduct={handleSelectProduct}
+                    selectedProducts={selectedProducts}
+                />
+                <OrderLocation onChangeLocation={location=>setOrderLocation(location)}/>
+                <OrderSummary 
+                    amount={selectedProducts.length} 
+                    totalPrice={totalPrice}
+                    onSubmit={handleSubmit}
+                />
+            </div>
+            <Footer/>
+        </>
     )
 }
 
